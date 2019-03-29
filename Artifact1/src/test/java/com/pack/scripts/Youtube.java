3852cc36528f399982c5f6d0bec15163005f6566
manalikamble <manalik@prdxn.com>
2019-03-29 17:35:06 +0530

    Changed Youtube code

--- Artifact1/src/test/java/com/pack/scripts/Youtube.java
+++ Artifact1/src/test/java/com/pack/scripts/Youtube.java
@@ -1,40 +1,31 @@
 package com.pack.scripts;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
-import org.sikuli.script.FindFailed;
-import org.sikuli.script.ImagePath;
-import org.sikuli.script.Pattern;
-import org.sikuli.script.Screen;
+	import org.openqa.selenium.By;
+	import org.openqa.selenium.WebDriver;
+	import org.openqa.selenium.WebElement;
+	import org.openqa.selenium.chrome.ChromeDriver;
+	import org.openqa.selenium.interactions.Actions;
+	import org.openqa.selenium.support.ui.ExpectedConditions;
+	import org.openqa.selenium.support.ui.WebDriverWait;
+	import org.testng.annotations.AfterClass;
+	import org.testng.annotations.BeforeClass;
+	import org.testng.annotations.Test;
+	import org.sikuli.script.FindFailed;
+	import org.sikuli.script.ImagePath;
+	import org.sikuli.script.Pattern;
+	import org.sikuli.script.Screen;
 
-public class Youtube {
-	
-
-	@Test
-	public void setup() throws InterruptedException
-	{
+	public class Youtube {
 		WebDriver driver;
-		System.setProperty("webdriver.chrome.driver", "E:\\chromedriver_win32\\chromedriver.exe");
-		driver=new ChromeDriver();
-                WebElement ele=driver.findElement(By.xpath("//*[@title='Play Video']"));
-		Thread.sleep(10000);
-		WebDriverWait wait = new WebDriverWait(driver, 20);
-		wait.until(ExpectedConditions.elementToBeClickable(ele));
-		ele.click();
 
-		Pattern Play = new Pattern("PlayButton.png");
-		//ImagePath //s;
-		//s.find(Play); //identify play button
-		
-		
-	}
-		
-	}       
+		@Test
+		public void setup() throws InterruptedException
+		{
+			
+			System.setProperty("webdriver.chrome.driver", "E:\\chromedriver_win32\\chromedriver.exe");
+			driver=new ChromeDriver();
+			driver.quit();
+			
+			
+		}
+}
