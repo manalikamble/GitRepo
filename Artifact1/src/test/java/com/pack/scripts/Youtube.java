adae010a3ef162982405769255170dfa3dfc7362
manalikamble <manalik@prdxn.com>
2019-03-29 12:48:15 +0530

    Commit by Manali

--- Artifact1/src/test/java/com/pack/scripts/Youtube.java
+++ Artifact1/src/test/java/com/pack/scripts/Youtube.java
@@ -16,51 +16,25 @@ import org.sikuli.script.Pattern;
 import org.sikuli.script.Screen;
 
 public class Youtube {
-	WebDriver driver;
+	
 
-	@BeforeClass
+	@Test
 	public void setup() throws InterruptedException
 	{
-		
+		WebDriver driver;
 		System.setProperty("webdriver.chrome.driver", "E:\\chromedriver_win32\\chromedriver.exe");
 		driver=new ChromeDriver();
-		
-		
-		
-	}
-	
-	@Test
-	public void testvideo() throws InterruptedException, FindFailed
-	{
-        Screen s=new Screen();
-		driver.get("https://events.economist.com/become-a-sponsor/");
-		/*WebElement ele=driver.findElement(By.xpath("//*[@title='Play Video']"));
-		//Thread.sleep(10000);
+                WebElement ele=driver.findElement(By.xpath("//*[@title='Play Video']"));
+		Thread.sleep(10000);
 		WebDriverWait wait = new WebDriverWait(driver, 20);
 		wait.until(ExpectedConditions.elementToBeClickable(ele));
-		ele.click();*/
-        //Thread.sleep(50000);
-		String filepath = "C:\\Users\\prdxn71\\eclipse-workspace\\Test\\Artifact1\\images\\";
-		Pattern Play = new Pattern(filepath + "PlayButton.PNG");
-        
-        //ImagePath.setBundlePath("fullpath");
-		//Pattern Play = new Pattern("PlayButton.png");
-		Thread.sleep(50000);
-		s.find(Play); //identify pause button
-		s.click(Play); //click pause button
-		System.out.println("pause button clicked");
-		System.out.println(ImagePath.getBundlePath());
-		s.find(Play); //identify play button
-		s.click(Play); //click play button
-		
-		
-		//*[@id=\"vjs_video_3\"]/div[5]"
-	}
-	
-	@AfterClass
-	public void teardown()
-	{
-		driver.quit();
-	}
-}
+		ele.click();
 
+		Pattern Play = new Pattern("PlayButton.png");
+		//ImagePath //s;
+		//s.find(Play); //identify play button
+		
+		
+	}
+		
+	}       
